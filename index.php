0cf60fc18911b501ccad3ad5459c9828a6c663cd
truelogic <gpaoli@truelogic.software>
2019-03-21 18:21:55 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,438 @@
+<?php
+
+  $newUrl = array(
+    'www.aeplay.tv/{pais}/',
+    'www.aeplay.tv/{pais}/horarios',
+    'www.aeplay.tv/{pais}/horarios',
+    'www.aeplay.tv/{pais}/movies/',
+    'latam.aeplay.tv/notas',
+    'www.aeplay.tv/{pais}/',
+    'latam.aeplay.tv/notas',
+    'latam.aeplay.tv/notas',
+    'latam.aeplay.tv/notas',
+    'latam.aeplay.tv/notas/{variable}',
+    'latam.aeplay.tv/movies/{variable}',
+    'latam.aeplay.tv/donde-vernos',
+    'www.aeplay.tv/{pais}/',
+    'www.aeplay.tv/{pais}/search',
+    'www.aeplay.tv/{pais}/',
+    'latam.aeplay.tv/series/{variable}',
+    'www.aeplay.tv/{pais}/programas/',
+    'www.aeplay.tv/{pais}/programas/',
+    'www.aeplay.tv/{pais}/programas/',
+    'latam.aeplay.tv/personaje/{variable}',
+    'latam.aeplay.tv/personaje/',
+    'latam.aeplay.tv/content/{variable}',
+    'www.aeplay.tv/{pais}/',
+    'www.aeplay.tv/{pais}/programas/',
+    'www.aeplay.tv/{pais}/programas/',
+    'www.aeplay.tv/{pais}/programas/',
+    'latam.aeplay.tv/juegos/{variable}',
+    'latam.aeplay.tv/encuesta/{variable}',
+    'latam.aeplay.tv/contacto',
+    'latam.aeplay.tv/quienes-somos',
+    'latam.aeplay.tv/donde-vernos',
+    'latam.aeplay.tv/terminos-uso',
+    'latam.aeplay.tv/políticas-autor',
+    'latam.aeplay.tv/políticas-privacidad',
+    'latam.aeplay.tv/rrpp-prensa',
+    'latam.aeplay.tv/publicidad',
+    'www.aeplay.tv',
+    'www.aeplay.tv/{pais}/programas/',
+    'www.aeplay.tv/{pais}/programas/',
+    'www.aeplay.tv/{pais}/horarios',
+    'www.aeplay.tv',
+    'latam.aeplay.tv/contacto',
+    'latam.aeplay.tv/quienes-somos',
+    'latam.aeplay.tv/microsites/{variable}',
+    'www.aeplay.tv/{pais}/serie/60-días-preso-el-experimento_nl0zjx',
+    'www.aeplay.tv/{pais}/serie/terapia-de-shock_leldln',
+    'www.aeplay.tv/{pais}/serie/hijas-de-la-poligamia_7bov9w',
+    'www.aeplay.tv/{pais}/movie/me-enamore-de-mi-alumno-la-historia-de-mary-key-letourneau_jgh84e',
+    'www.aeplay.tv/{pais}/movie/el-secuestro-de-elizabeth-smart-parte-1_dqoqgh',
+    'www.aeplay.tv/{pais}/serie/lo-mejor-de-policias-en-vivo_0ib2z8',
+    'www.aeplay.tv/{pais}/',
+    'www.aeplay.tv/{pais}/',
+    'www.aeplay.tv/{pais}/',
+    'www.aeplay.tv/{pais}/',
+    'www.aeplay.tv/{pais}/serie/acumuladores-compulsivos',
+    'www.aeplay.tv/{pais}/serie/demasiado-joven-para-matar',
+    'www.aeplay.tv/{pais}/serie/esclavos-de-la-cienciologia',
+    'www.aeplay.tv/{pais}/serie/ncis-new-orleans',
+    'www.aeplay.tv/{pais}/serie/los-hermanos-menendez-la-historia-jamas-contada'
+  );
+
+
+  $oldUrl = array(
+    '{pais}.canalaetv.com',
+    '{pais}.canalaetv.com/horarios-programas',
+    '{pais}.canalaetv.com/horarios-programas/{variable}',
+    '{pais}.canalaetv.com/movies',
+    '{pais}.canalaetv.com/aextras',
+    '{pais}.canalaetv.com/videos',
+    '{pais}.canalaetv.com/etiquetas/{variable}',
+    '{pais}.canalaetv.com/etiquetas/',
+    '{pais}.canalaetv.com/noticias/',
+    '{pais}.canalaetv.com/noticias/{variable}',
+    '{pais}.canalaetv.com/movies/{variable}',
+    '{pais}.canalaetv.com/content/donde-vernos',
+    '{pais}.canalaetv.com/google_vignette',
+    '{pais}.canalaetv.com/search/node',
+    '{pais}.canalaetv.com/videos/{variable}',
+    '{pais}.canalaetv.com/series/{variable}',
+    '{pais}.canalaetv.com/series/',
+    '{pais}.canalaetv.com/episode/{variable}',
+    '{pais}.canalaetv.com/episode/',
+    '{pais}.canalaetv.com/personaje/{variable}',
+    '{pais}.canalaetv.com/personaje/',
+    '{pais}.canalaetv.com/content/{variable}',
+    '{pais}.canalaetv.com/content/',
+    '{pais}.canalaetv.com/programas/',
+    '{pais}.canalaetv.com/programas/episodios/{variable}',
+    '{pais}.canalaetv.com/programas/episodios/',
+    '{pais}.canalaetv.com/juegos/{variable}',
+    '{pais}.canalaetv.com/encuesta/{variable}',
+    '{pais}.canalaetv.com/content/contacto',
+    '{pais}.canalaetv.com/quienes_somos',
+    '{pais}.canalaetv.com/content/donde-vernos',
+    '{pais}.canalaetv.com/terminos_de_uso',
+    '{pais}.canalaetv.com/politicas_derechos_autor',
+    '{pais}.canalaetv.com/politicas_de_privacidad',
+    '{pais}.canalaetv.com/rrpp_y_prensa',
+    '{pais}.canalaetv.com/publicidad',
+    'play.canalaetv.com',
+    'play.canalaetv.com/#/featured',
+    'play.canalaetv.com/#/shows',
+    'play.canalaetv.com/#/tv-schedule',
+    'play.canalaetv.com/#/notifications',
+    'play.canalaetv.com/#/help',
+    'play.canalaetv.com/#/aboutus',
+    '{pais}.canalaetv.com/microsites/{variable}',
+    '{pais}.canalaetv.com/microsites/aeinvestiga/show/60-dias-preso--el-experimento',
+    '{pais}.canalaetv.com/microsites/demasiado-joven-para-matar/show/terapia-de-shock',
+    '{pais}.canalaetv.com/microsites/aeinvestiga/show/hijas-de-la-poligamia',
+    '{pais}.canalaetv.com/microsites/aeinvestiga/show/me-enamore-de-mi-alumno--la-historia-de-mary-kay-letourneau',
+    '{pais}.canalaetv.com/microsites/aeinvestiga/show/el-secuestro-de-elizabeth-smart',
+    '{pais}.canalaetv.com/microsites/live-pd-policias-en-vivo/show/lo-mejor-de-policias-en-vivo',
+    '{pais}.canalaetv.com/microsites/talent-land',
+    '{pais}.canalaetv.com/microsites/somos-justicia',
+    '{pais}.canalaetv.com/microsites/hablemosdebullying',
+    '{pais}.canalaetv.com/microsites/aesocial',
+    '{pais}.canalaetv.com/series/acumuladores-compulsivos',
+    '{pais}.canalaetv.com/microsites/demasiado-joven-para-matar/show/demasiado-joven-para-matar',
+    '{pais}.canalaetv.com/series/esclavos-de-la-cienciologia',
+    '{pais}.canalaetv.com/series/ncis-new-orleans-1',
+    '{pais}.canalaetv.com/series/los-hermanos-menendez-la-historia-jamas-contada'
+  );
+
+  $exceptions = array(
+    '{pais}.canalaetv.com/microsites/aeinvestiga/show/60-dias-preso--el-experimento',
+    '{pais}.canalaetv.com/microsites/demasiado-joven-para-matar/show/terapia-de-shock',
+    '{pais}.canalaetv.com/microsites/aeinvestiga/show/hijas-de-la-poligamia',
+    '{pais}.canalaetv.com/microsites/aeinvestiga/show/me-enamore-de-mi-alumno--la-historia-de-mary-kay-letourneau',
+    '{pais}.canalaetv.com/microsites/aeinvestiga/show/el-secuestro-de-elizabeth-smart',
+    '{pais}.canalaetv.com/microsites/live-pd-policias-en-vivo/show/lo-mejor-de-policias-en-vivo',
+    '{pais}.canalaetv.com/microsites/talent-land',
+    '{pais}.canalaetv.com/microsites/somos-justicia',
+    '{pais}.canalaetv.com/microsites/hablemosdebullying',
+    '{pais}.canalaetv.com/microsites/aesocial',
+    '{pais}.canalaetv.com/series/acumuladores-compulsivos',
+    '{pais}.canalaetv.com/microsites/demasiado-joven-para-matar/show/demasiado-joven-para-matar',
+    '{pais}.canalaetv.com/series/esclavos-de-la-cienciologia',
+    '{pais}.canalaetv.com/series/ncis-new-orleans-1',
+    '{pais}.canalaetv.com/series/los-hermanos-menendez-la-historia-jamas-contada',
+    '{pais}.canalaetv.com/series/fronteras-peligrosas'
+  );
+
+  function isPlayUrl($url){
+    $parts = explode('play.',$url);
+    if(sizeof($parts) > 1):
+      //return str_replace('',"{pais}.",$url);
+      return true;
+    endif;
+  }
+
+  function isException($url, $countryInUrl, $exceptions){
+    $url = convertUrlException($url, $countryInUrl, $countries);
+    $url = removeHtpps($url);
+
+    if(in_array($url, $exceptions)):
+      return $url;
+    else:
+      return false;
+    endif;
+  }
+
+
+  function convertUrlException($url, $countryInUrl, $countries){
+    if($countryInUrl):
+      $url = str_replace($countryInUrl .'.',"{pais}.",$url);
+    endif;
+    return $url;
+  }
+
+  $countries = array(
+    "ar",
+    "br",
+    "bo",
+    "cl",
+    "co",
+    "cr",
+    "do",
+    "ec",
+    "gt",
+    "hn",
+    "mx",
+    "ni",
+    "pa",
+    "pe",
+    "py",
+    "sv",
+    "uy",
+    "ve"
+  );
+
+  $urlsWithVariables = array(
+    '/horarios-programas/',
+    '/etiquetas/',
+    '/noticias/',
+    '/movies/',
+    '/videos/',
+    '/series/',
+    '/episode/',
+    '/personaje/',
+    '/content/',
+    '/episodios/',
+    '/juegos/',
+    '/encuesta/',
+    '/microsites/',
+    '/programas/'
+  );
+
+  function strposa($haystack, $needles=array(), $offset=0) {
+    $chr = array();
+    foreach($needles as $needle) {
+      $res = strpos($haystack, $needle, $offset);
+      if ($res !== false) $chr[$needle] = $res;
+    }
+    if(empty($chr)) return false;
+      
+    $parts = explode(key($chr), $haystack);
+    return $parts[1];
+  }
+
+  function removeHtpps($url){
+    $url = str_replace("https://","",$url);
+    $url = str_replace("http://","",$url);
+    return $url;
+  }
+
+  function countryInUrl($url, $countries){
+    $url = removeHtpps($url);
+    $parts = explode(".", $url);
+    if (in_array($parts[0], $countries)):
+      return $parts[0];
+    endif;
+    return null;
+  }
+
+  function variableInUrl($url, $urlsWithVariables){
+    $strposaUrl = strposa($url, $urlsWithVariables, 1);
+    if ($strposaUrl):
+      if($strposaUrl !== ''):
+        return $strposaUrl;
+      else:
+        return null;
+      endif;
+    else:
+      return null;
+    endif;
+  }
+
+  function mergeOldUrlAndNewUrl($oldUrl, $newUrl){
+    $oldUrl = array_values($oldUrl);
+    $newUrl = array_values($newUrl);
+
+    foreach($oldUrl as $key => $oldUrlInd):
+      $mergedUrls[] = array('oldUrl' => $oldUrlInd, 'newUrl' => $newUrl[$key]); 
+    endforeach;
+
+    return $mergedUrls;
+  }
+
+  function existParsedUrlInArrayOldUrl($url, $oldUrl){
+    if (in_array($url, $oldUrl)):
+      return $url;
+    else:
+      return null;
+    endif;
+  }
+
+  function matchNewUrl($url, $oldUrl, $newUrl){
+    $mergeOldUrlAndNewUrl = mergeOldUrlAndNewUrl($oldUrl, $newUrl);
+    foreach($mergeOldUrlAndNewUrl as $mergeOldUrlAndNewUrlInn):
+      if($url == $mergeOldUrlAndNewUrlInn['oldUrl']):
+        return $mergeOldUrlAndNewUrlInn['newUrl'];
+      endif;
+    endforeach;
+  }
+
+  function convertUrl($url, $countryInUrl, $variableInUrl){
+    if($countryInUrl):
+      $url = str_replace($countryInUrl .'.',"{pais}.",$url);
+    endif;
+    if($variableInUrl):
+      $url = str_replace($variableInUrl,"{variable}",$url);
+    endif;
+    return $url;
+  }
+
+  function reverseConvertUrl($url, $countryInUrl, $variableInUrl){
+    if($countryInUrl):
+      $url = str_replace("{pais}",$countryInUrl,$url);
+    endif;
+    if($variableInUrl):
+      $url = str_replace("{variable}",$variableInUrl,$url);
+    endif;
+    return $url;
+  }
+
+  function addHttps($url){
+    $url = 'https://' . $url;
+    return $url;
+  }
+
+  function parseUrl($url, $countryInUrl, $variableInUrl, $oldUrl, $newUrl, $exceptions){
+
+    //Si es una excepcion
+
+    if(isPlayUrl($url)):
+      //OJO, LA URL VIENE SIN EL PAIS.
+      $url = matchNewUrl($url, $oldUrl, $newUrl);
+      $url = str_replace('{pais}', 'ar',$url);
+      $url = addHttps($url);
+      var_dump('La url final: ' . $url);
+      return $url;
+    endif;
+
+
+    $isException = isException($url, $countryInUrl, $exceptions);
+    if($isException):
+      $url = matchNewUrl($isException, $oldUrl, $newUrl);
+      $url = reverseConvertUrl($url, $countryInUrl, $variableInUrl);
+      $url = addHttps($url);
+    
+      var_dump('La url final: ' . $url);
+      return $url;
+    endif;
+
+    //Si no es una excepcion
+    $url = convertUrl($url, $countryInUrl, $variableInUrl);
+    $url = removeHtpps($url);
+    $url = existParsedUrlInArrayOldUrl($url, $oldUrl);
+    $url = matchNewUrl($url, $oldUrl, $newUrl);
+    $url = reverseConvertUrl($url, $countryInUrl, $variableInUrl);
+
+    //Si la url no esta en el array hay que ver que hacemos
+
+    $url = addHttps($url);
+    var_dump('La url final: ' . $url);
+
+    return $url;
+  }
+
+?>
+
+
+<?php 
+  if(isset($_POST['url'])):
+    $url = $_POST['url'];
+    $countryInUrl = countryInUrl($url, $countries);
+    $variableInUrl = variableInUrl($url, $urlsWithVariables);
+    parseUrl($url, $countryInUrl, $variableInUrl, $oldUrl, $newUrl, $exceptions);
+  endif;
+?>
+
+
+<form action="index.php" method="post">
+  URL:
+  <input type="text" name="url" value="">
+  <input type="submit" value="Submit">
+</form> 
+
+
+<?php
+
+
+$test = array(
+  'ar.canalaetv.com',
+  'ar.canalaetv.com/horarios-programas',
+  'ar.canalaetv.com/horarios-programas/algo',
+  'ar.canalaetv.com/movies',
+  'ar.canalaetv.com/aextras',
+  'ar.canalaetv.com/videos',
+  'ar.canalaetv.com/etiquetas/algo',
+  'ar.canalaetv.com/etiquetas/',
+  'ar.canalaetv.com/noticias/',
+  'ar.canalaetv.com/noticias/algo',
+  'ar.canalaetv.com/movies/algo',
+  'ar.canalaetv.com/content/donde-vernos',
+  'ar.canalaetv.com/google_vignette',
+  'ar.canalaetv.com/search/node',
+  'ar.canalaetv.com/videos/algo',
+  'ar.canalaetv.com/series/algo',
+  'ar.canalaetv.com/series/',
+  'ar.canalaetv.com/episode/algo',
+  'ar.canalaetv.com/episode/',
+  'ar.canalaetv.com/personaje/algo',
+  'ar.canalaetv.com/personaje/',
+  'ar.canalaetv.com/content/algo',
+  'ar.canalaetv.com/content/',
+  'ar.canalaetv.com/programas/',
+  'ar.canalaetv.com/programas/episodios/algo',
+  'ar.canalaetv.com/programas/episodios/',
+  'ar.canalaetv.com/juegos/algo',
+  'ar.canalaetv.com/encuesta/algo',
+  'ar.canalaetv.com/content/contacto',
+  'ar.canalaetv.com/quienes_somos',
+  'ar.canalaetv.com/content/donde-vernos',
+  'ar.canalaetv.com/terminos_de_uso',
+  'ar.canalaetv.com/politicas_derechos_autor',
+  'ar.canalaetv.com/politicas_de_privacidad',
+  'ar.canalaetv.com/rrpp_y_prensa',
+  'ar.canalaetv.com/publicidad',
+  'play.canalaetv.com',
+  'play.canalaetv.com/#/featured',
+  'play.canalaetv.com/#/shows',
+  'play.canalaetv.com/#/tv-schedule',
+  'play.canalaetv.com/#/notifications',
+  'play.canalaetv.com/#/help',
+  'play.canalaetv.com/#/aboutus',
+  'ar.canalaetv.com/microsites/algo',
+  'ar.canalaetv.com/microsites/aeinvestiga/show/60-dias-preso--el-experimento',
+  'ar.canalaetv.com/microsites/demasiado-joven-para-matar/show/terapia-de-shock',
+  'ar.canalaetv.com/microsites/aeinvestiga/show/hijas-de-la-poligamia',
+  'ar.canalaetv.com/microsites/aeinvestiga/show/me-enamore-de-mi-alumno--la-historia-de-mary-kay-letourneau',
+  'ar.canalaetv.com/microsites/aeinvestiga/show/el-secuestro-de-elizabeth-smart',
+  'ar.canalaetv.com/microsites/live-pd-policias-en-vivo/show/lo-mejor-de-policias-en-vivo',
+  'ar.canalaetv.com/microsites/talent-land',
+  'ar.canalaetv.com/microsites/somos-justicia',
+  'ar.canalaetv.com/microsites/hablemosdebullying',
+  'ar.canalaetv.com/microsites/aesocial',
+  'ar.canalaetv.com/series/acumuladores-compulsivos',
+  'ar.canalaetv.com/microsites/demasiado-joven-para-matar/show/demasiado-joven-para-matar',
+  'ar.canalaetv.com/series/esclavos-de-la-cienciologia',
+  'ar.canalaetv.com/series/ncis-new-orleans-1',
+  'ar.canalaetv.com/series/los-hermanos-menendez-la-historia-jamas-contada'
+);
+
+
+  foreach($test as $testInd):
+    $url = $testInd;
+    var_dump("url vieja: " . $testInd);
+    $countryInUrl = countryInUrl($url, $countries);
+    $variableInUrl = variableInUrl($url, $urlsWithVariables);
+    parseUrl($url, $countryInUrl, $variableInUrl, $oldUrl, $newUrl, $exceptions);
+  endforeach;
+
+?>
